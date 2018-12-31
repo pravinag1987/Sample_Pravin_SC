61d712154414939ee31c7f42e4709b1f5e476fec
pravinag <ambrosepravin1987@gmail.com>
2018-12-31 11:55:29 +0530

    Master project

---
+++ src/main/java/scala/SampleScala_code.scala
@@ -0,0 +1,34 @@
+package scala
+
+object SampleScala_code {
+
+  def main(args: Array[String]): Unit = {
+
+    var  ff = new testcls()
+
+   var lll = ff.test11
+
+    print(lll)
+
+  }
+
+
+}
+
+class testcls
+{
+  def test11: Int = {
+
+
+    var dd = 25
+
+    var  dd1 = 30
+
+    var kkk = dd + dd1
+
+   // println(kkk)
+
+    return kkk
+
+  }
+}
---
+++ src/main/java/test/scala/SampleScala.scala
@@ -0,0 +1,25 @@
+package test.scala
+
+import org.testng.Assert
+import org.testng.annotations.Test
+
+
+class SampleScala
+{
+
+  @Test
+  def testEmailGenerator(): Unit = {
+
+    var ddd = new scala.testcls().test11
+
+
+    Assert.assertNotNull(ddd)
+    Assert.assertEquals(ddd, 55)
+  }
+
+
+
+
+
+}
+
